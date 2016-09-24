06c7f31ae87cdfafa3ddee0e04273176a3942aeb
xiongxiong109 <444122662@qq.com>
2016-09-24 17:38:24 +0800

    oper with array

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './with_redux/hello.redux'
+import App from './with_redux/oper.array'
 
 export default App
---
+++ with_redux/oper.array.js
@@ -0,0 +1,69 @@
+/*
+	使用redux reducer的时候对数组数据的操作
+	仍然是pure function的原则, 我们不再希望reducer对数组执行增删改操作的时候, 改变了之前的state,
+	所以我们将不再推荐使用数组的push、splice方法(这两个方法都会改变原来的数组)
+	推荐使用的是concat、slice、以及es6的...
+*/
+
+import React from 'react'
+import { combineReducers, createStore, dispatch } from 'redux'
+
+const arrayReducer = (state = [], action) => {
+	switch(action.type) {
+		case 'PUSH': // 在数组的最后插入一个元素action = {type: 'PUSH', num}
+			return [
+				...state,
+				action.num
+			]
+		break;
+		case 'INSERT': // 在数组的任意位置插入一个元素 action = {type: 'INSERT', num, idx}
+			return [
+				...state.slice(0, action.idx),
+				action.num,
+				...state.slice(action.idx)
+			]
+		break;
+		case 'DELETE':
+			return state.filter(n => n !== action.num)
+		break;
+		default:
+			return state;
+	}
+}
+
+let store = createStore(arrayReducer);
+store.subscribe(() => {
+	console.log(store.getState())
+})
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			rst: []
+		}
+	}
+
+	componentWillMount() {
+		store.dispatch({type: 'PUSH', num: 8}); // [8]
+		store.dispatch({type: 'PUSH', num: 12}); // [8, 12]
+		store.dispatch({type: 'INSERT', num: 66, idx: 1}); // [8, 66, 12]
+		store.dispatch({type: 'INSERT', num: 88, idx: 2}); // [8, 66, 88, 12]
+		store.dispatch({type: 'DELETE', num: 88}); // [8, 66, 12]
+	}
+
+	componentDidMount() {
+		this.setState({
+			rst: store.getState()
+		})
+	}
+
+	render() {
+		return (
+			<div className="counter-wrap">
+				<p>{this.state.rst.join(', ')}</p>
+			</div>
+		)
+	}
+}
+
+export default App
