2ef41cf54d6e713439a7b33fdb3839ad3645f60d
xiongxiong109 <444122662@qq.com>
2016-09-25 17:27:41 +0800

    add mocha test

---
+++ test/test.hello.js
@@ -0,0 +1,10 @@
+/*测试模块*/
+var expect = require('chai').expect;
+
+describe('测试模块', function () {
+
+	it('show return hello mocha', function () {
+		expect('hello mocha').to.equal('hello mocha')
+	})
+
+})
---
+++ test/test.todoReducer.js
@@ -0,0 +1,56 @@
+/*测试 todoReducer */
+
+"use strict"
+
+const expect = require('chai').expect;
+const Redux = require('redux');
+
+var id = 0;
+
+var action = {type: 'ADD_TODO', id: 0, text: 'hello redux'}
+
+function todoReducer (state, action) {
+
+	state = state || [];
+
+	switch(action.type) {
+		case 'ADD_TODO':
+			return state.concat(
+					{
+						id: action.id,
+						text: action.text
+					}
+				);
+		break;
+		default:
+			return state
+	}
+
+}
+
+let store = Redux.createStore(todoReducer);
+
+var stateBefore = [];
+var stateAfter = [
+	{
+		id: 0,
+		text: 'hello redux'
+	}
+]
+
+describe('todo reducer', () => {
+
+	beforeEach(function() {
+		store.dispatch(action)
+		stateBefore = store.getState()
+	})
+
+	it('should be equal', () => {
+		stateAfter.forEach((item, idx) => {
+			for (var key in item) {
+				expect(item[key]).to.equal(stateBefore[idx][key])
+			}
+		})
+	})
+
+})
