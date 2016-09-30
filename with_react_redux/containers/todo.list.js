bb96e3cb6bd69a590ea5f03e254c1db03bb8721f
xiongxiong109 <444122662@qq.com>
2016-09-30 11:29:29 +0800

    withRouter

--- with_react_redux/containers/todo.list.js
+++ with_react_redux/containers/todo.list.js
@@ -1,5 +1,7 @@
 // list container
+import React from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 import { addAction, toggleItem } from '../action'
 import { TodoList } from '../components'
 
@@ -23,10 +25,13 @@ const getVisibleLists = (state, filter) => {
 
 }
 
-// 第二个参数可以传入组件自身的属性值，通过获取组件自身的属性值来代替state中的属性
-const mapListStateToProps = (state, ownProps) => {
+/*
+	第二个参数可以传入组件自身的属性值，通过获取组件自身的属性值来代替state中的属性
+	然后也可以通过es6的写法,来萃取filter属性的值
+*/
+const mapListStateToProps = (state, {filter}) => {
 	return {
-		lists: getVisibleLists(state, ownProps.filter)
+		lists: getVisibleLists(state, filter)
 	}
 }
 
@@ -41,12 +46,11 @@ const mapListStateToProps = (state, ownProps) => {
 // 	}
 // }
 
-const ListContainer = 
-	connect(
+const ListContainer = withRouter(connect(
 		mapListStateToProps,
 		// mapListDispatchToProps
 		// 如果mapDispatchToProps里面的各种方法都是一个纯函数, 那么在connect中就可以直接一波带走
 		{ toggleState: toggleItem,  addData: addAction}
-	)(TodoList)
+	)(TodoList), {withRef: true});
 
 export default ListContainer
