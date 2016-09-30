0f955dbfe341e1ed809887d023e529ceb015e24c
xiongxiong109 <444122662@qq.com>
2016-09-30 10:56:05 +0800

    简化mapDispathToProps方法的描述

--- with_react_redux/containers/todo.list.js
+++ with_react_redux/containers/todo.list.js
@@ -23,27 +23,30 @@ const getVisibleLists = (state, filter) => {
 
 }
 
-// 第二个参数传入自身的属性，来代替state中的属性
+// 第二个参数可以传入组件自身的属性值，通过获取组件自身的属性值来代替state中的属性
 const mapListStateToProps = (state, ownProps) => {
 	return {
 		lists: getVisibleLists(state, ownProps.filter)
 	}
 }
 
-const mapListDispatchToProps = (dispatch) => {
-	return {
-		toggleState(id) {
-			dispatch(toggleItem(id))
-		},
-		addData(str) {
-			dispatch(addAction(str))
-		}
-	}
-}
+// const mapListDispatchToProps = (dispatch) => {
+// 	return {
+// 		toggleState(id) {
+// 			dispatch(toggleItem(id))
+// 		},
+// 		addData(str) {
+// 			dispatch(addAction(str))
+// 		}
+// 	}
+// }
 
-const ListContainer = connect(
-	mapListStateToProps,
-	mapListDispatchToProps
-)(TodoList)
+const ListContainer = 
+	connect(
+		mapListStateToProps,
+		// mapListDispatchToProps
+		// 如果mapDispatchToProps里面的各种方法都是一个纯函数, 那么在connect中就可以直接一波带走
+		{ toggleState: toggleItem,  addData: addAction}
+	)(TodoList)
 
 export default ListContainer
