180603c80efc77a784d00438cfd94bf471dce507
xiongxiong109 <444122662@qq.com>
2016-10-09 09:23:02 +0800

    add es6 for mocha

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # learning_react
 learning with react
+
+### 编写mocha 测试时的注意事项
+1. 如果需要在mocha中使用es6语法, 则需要安装 babel-core, 以及相应的babel-presets
+比如一般会安装presets-2015, 如果要测试react jsx语法, 则还需要指定presets-react
+2. 在项目目录下配置好相应的.babelrc
+内容如下:
+
+```json
+	{
+		"presets": ["es2015", "react"]
+	}
+```
+
+3. 执行对应的mocha指令
+
+```bash
+	cd ./test/ && mocha --compilers js:babel-core/register ./
+```
+
+4. 把测试命令写入到`package.json`中的`npm test`去
---
+++ test/searchApp/search.input.test.js
@@ -0,0 +1,20 @@
+// 对输入组件进行测试
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from '../../examples/searchApp/reducers'
+import SearchIptContainer from '../../examples/searchApp/containers/search.input'
+import { expect } from 'chai'
+describe('test for searchIpt component', () => {
+
+	let store;
+	before(() => {
+		store = createStore(reducers);
+	})
+
+	it('should be empty string value when nothing', () => {
+		let { searchValue } = store.getState()
+		expect(searchValue).to.be.empty
+	})
+
+});
