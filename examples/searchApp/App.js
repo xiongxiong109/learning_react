1d4b7721a1ae00839b58cbbbfb815b8c88f1f92d
xiongxiong109 <444122662@qq.com>
2016-10-10 11:59:04 +0800

    update css

--- examples/searchApp/App.js
+++ examples/searchApp/App.js
@@ -3,6 +3,8 @@ import SearchIptContainer from './containers/search.input'
 import SearchRstContainer from './containers/search.rst'
 import SearchStoreContainer from './containers/search.store'
 
+require('./style.css')
+
 export const SearchApp = () => (
 	<div className="search-app">
 		<SearchIptContainer />
