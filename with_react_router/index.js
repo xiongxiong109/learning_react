f4efbed2f44490ac1c3cfed50680ea6c5f5cf5b0
xiongxiong109 <444122662@qq.com>
2016-10-11 22:47:10 +0800

    log router urls

--- with_react_router/index.js
+++ with_react_router/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Router, Route, browserHistory } from 'react-router'
 
+let github = 'https://github.com/reactjs/react-router-tutorial/tree/master/lessons'
+let blog = 'http://www.ruanyifeng.com/blog/2016/05/react_router.html'
+
 const HelloComponent = () => (
-	<div>Hello <a href="https://github.com/reactjs/react-router-tutorial/tree/master/lessons">React Router</a></div>
+	<div>
+		<p>Hello <a href={github}>React Router</a></p>
+		<p><a href={blog}>ruan's blog</a></p>
+	</div>
 )
 
 const App = () => (
