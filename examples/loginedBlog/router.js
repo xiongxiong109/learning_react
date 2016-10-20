a1667a5833d4190b49e6d453b252adf40758f2aa
xiongxiong109 <444122662@qq.com>
2016-10-20 10:09:31 +0800

    default router

--- examples/loginedBlog/router.js
+++ examples/loginedBlog/router.js
@@ -1,12 +1,31 @@
 import { browserHistory, Router } from 'react-router'
 
-const rootRouter = {
-	path: '/',
-	component: require('./component/App'), // 这里不使用indexRoute, 直接在nav中使用IndexLink 指定to="/"
-	childRoutes: [
-		require('./routes/Login'),
-		require('./routes/Blog')
-	]
-}
+// const rootRouter = {
+// 	path: '/',
+// 	component: require('./component/App'), // 这里不使用indexRoute, 直接在nav中使用IndexLink 指定to="/"
+// 	childRoutes: [
+// 		require('./routes/Login'),
+// 		require('./routes/Blog')
+// 	]
+// }
+
+// 配置路由的时候传递一个数组进去，在数组的第二个参数里面可以设置默认路由，处理404
+const rootRouter = [
+	{
+		path: '/',
+		component: require('./component/App'), // 这里不使用indexRoute, 直接在nav中使用IndexLink 指定to="/"
+		childRoutes: [
+			require('./routes/Login'),
+			require('./routes/Blog')
+		]
+	},
+	{ // default router
+		path: '*',
+		component: require('./component/App'),
+		onEnter(router, redirect) {
+			redirect('/')
+		}
+	}
+]
 
 export default rootRouter
